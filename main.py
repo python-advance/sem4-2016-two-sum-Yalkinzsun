85a0b15637cf5f202b951d46b3d79e26613e4248
Kirill Skorobogatov <Kirill9806@gmail.com>
2018-12-01 21:13:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,6 @@
 """
         Two sum from https://leetcode.com/problems/two-sum
 """
-
-# TODO
-
-# Сделать хэш-таблицу с ключами по индексам, т.е.
-
-# {0:7, 1:9, ... }
-
-# Реализовать one pass решение из leetcode
-
-# Оценить работу программы с использованием структур из
-# collections
-
-
 class Solution:
 
     def deco(func):
@@ -67,28 +54,6 @@ class Solution:
 
 
 Solution.two_sum_brute([2, 7, 11, 15], 9)
-
 assert Solution.two_sum_brute([2, 7, 11, 15], 9) == [0, 1], 'проверка случая с единственным сочетанием элементов'
-
 Solution.two_sum([2, 7, 11, 15], 9)
-
 assert Solution.two_sum([2, 7, 11, 15], 9) == [0, 1], 'проверка случая с единственным сочетанием элементов'
-
-if __name__ == '__main__':
-    import timeit
-
-    # print(timeit.timeit("test()", setup="from __main__ import test"))
-
-# Given nums = [2, 7, 11, 15], target = 9,
-#
-# Because nums[0] + nums[1] = 2 + 7 = 9,
-# return [0, 1].
-
-# Шаги решения:
-# 1. Решаем "влоб", оцениваем время работы.
-# 2. timeit
-# 2a. Декоратор
-# 3. Решаем с хэш-таблицей (словарь)
-# 4. Улучшение с помощью модуля collections?
-# 5. Замеряем время работы
-# 6. Делаем выводы
